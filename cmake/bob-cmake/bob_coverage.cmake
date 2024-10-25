aff37762db4ac29cf386b40aca202f400648b0df
Rene Moll <github@r-moll.nl>
2024-10-25 22:49:51 +0200

    build: add LLVM coverage report generation

--- cmake/bob-cmake/bob_coverage.cmake
+++ cmake/bob-cmake/bob_coverage.cmake
@@ -2,14 +2,16 @@
 option(BOB_COVERAGE "Enable code coverage target creation" Off)
 
 if (BOB_COVERAGE)
-	find_program(GCOVR_PATH gcovr)
-
 	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 		#
 		# Following the instructions from: https://clang.llvm.org/docs/SourceBasedCodeCoverage.html
 		#
 
 		bob_info("generating llvm coverage report.")
+
+		find_program(LLVM_PROFDATA_APP llvm-profdata)
+		find_program(LLVM_COV_APP llvm-cov)
+
 		add_compile_options(
 			# -fprofile-arcs
 			# -ftest-coverage
@@ -27,6 +29,9 @@ if (BOB_COVERAGE)
 		#
 
 		bob_info("generating gcov coverage report.")
+
+		find_program(GCOVR_APP gcovr)
+
 		add_compile_options(
 			-O0												# Disable optimizations when generating test-coverage
 			--coverage										# Synonym for -fprofile-arcs -ftest-coverage & -lgcov
@@ -53,19 +58,51 @@ function(bob_create_coverage_report_target)
 	set(output_folder "${PROJECT_BINARY_DIR}/coverage_${arg_NAME}")
 	bob_info("generating coverage report in: ${output_folder}")
 
-	add_custom_target(
-		${arg_NAME}
-		COMMAND
-			${arg_RUNNER}
-		COMMAND
-			${CMAKE_COMMAND} -E make_directory ${output_folder}
-		COMMAND
-			${GCOVR_PATH} -r ${PROJECT_SOURCE_DIR} --html-details --output "${output_folder}/index.html"
-		COMMAND
-			${GCOVR_PATH} -r ${PROJECT_SOURCE_DIR}
-		WORKING_DIRECTORY
-			${PROJECT_BINARY_DIR}
-		DEPENDS
-			${arg_RUNNER}
-	)
+	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+		add_custom_target(
+			${arg_NAME}
+			COMMAND
+				${CMAKE_COMMAND} -E env LLVM_PROFILE_FILE="${arg_NAME}.profraw" $<TARGET_FILE:${arg_RUNNER}>
+			COMMAND
+				${CMAKE_COMMAND} -E make_directory ${output_folder}
+			COMMAND
+				${LLVM_PROFDATA_APP} merge -sparse "${arg_NAME}.profraw" -o "${arg_NAME}.profdata"
+			COMMAND
+				${LLVM_COV_APP} show
+					-ignore-filename-regex=".*[/\]tests[/\].*"
+					-show-mcdc
+					-show-line-counts-or-regions
+					-format=html
+					-instr-profile="${arg_NAME}.profdata"
+					$<TARGET_FILE:${arg_RUNNER}>
+					> "${output_folder}/index.html"
+			COMMAND
+				${LLVM_COV_APP} report
+					-ignore-filename-regex=".*[/\]tests[/\].*"
+					-show-mcdc-summary
+					-instr-profile="${arg_NAME}.profdata"
+					$<TARGET_FILE:${arg_RUNNER}>
+					> "${output_folder}/report.txt"
+			WORKING_DIRECTORY
+				${PROJECT_BINARY_DIR}
+			DEPENDS
+				${arg_RUNNER}
+		)
+	else()
+		add_custom_target(
+			${arg_NAME}
+			COMMAND
+				${arg_RUNNER}
+			COMMAND
+				${CMAKE_COMMAND} -E make_directory ${output_folder}
+			COMMAND
+				${GCOVR_APP} -r ${PROJECT_SOURCE_DIR} --html-details --output "${output_folder}/index.html"
+			COMMAND
+				${GCOVR_APP} -r ${PROJECT_SOURCE_DIR}
+			WORKING_DIRECTORY
+				${PROJECT_BINARY_DIR}
+			DEPENDS
+				${arg_RUNNER}
+		)
+	endif()
 endfunction()
