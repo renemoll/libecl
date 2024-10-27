c9f418f1d8565f8d03e516efcc73238d68d0f1a2
Rene Moll <github@r-moll.nl>
2024-10-27 20:49:52 +0100

    feat: introduce basic fifo container

--- common/include/containers/fifo.hpp
+++ common/include/containers/fifo.hpp
@@ -6,18 +6,26 @@
 #include <cstddef>
 #include <type_traits>
 
-namespace ecl_common {
+/*!
+ * FIFO buffer with fixed capacity.
+ * Single producer, single consumer.
+ *
+ * When full, new entry are not accepted until space is made by popping the
+ * oldest entries.
+s */
+
+namespace ecl::common {
 template <typename T, std::size_t N>
 class Fifo
 {
 public:
-	using container_type = std::array<T, N>;
-	using value_type = container_type::value_type;
-	using size_type = container_type::size_type;
+	using container_type = typename std::array<T, N>;
+	using value_type = typename container_type::value_type;
+	using size_type = typename container_type::size_type;
 	using pointer = size_type;
 	using const_pointer = size_type;
-	using reference = container_type::reference;
-	using const_reference = container_type::const_reference;
+	using reference = typename container_type::reference;
+	using const_reference = typename container_type::const_reference;
 
 	size_type capacity() const
 	{
@@ -47,45 +55,62 @@ public:
 
 	reference front()
 	{
+		assert(!empty());
 		return m_buffer[m_read.load()];
 	}
 
 	const_reference front() const
 	{
+		assert(!empty());
 		return m_buffer[m_read.load()];
 	}
 
-	void push(const value_type& value)
+	bool push(const value_type& value)
 	{
-		// TODO: check available space
-		const auto index = m_write.load();
-		const pointer next_index = (index + 1) % N;
-		m_buffer[index] = value;
-		m_write.store(next_index);
-		m_full = next_index == m_read.load();
+		if (m_full) {
+			return false;
+		}
+
+		const auto write = m_write.load();
+		const auto read = m_read.load();
+		const pointer next_write = (write + 1) % N;
+		m_buffer[write] = std::move(value);
+		m_write.store(next_write);
+		m_full = next_write == read;
+		return true;
 	}
 
-	void push(value_type&& value)
+	bool push(value_type&& value)
 	{
-		// TODO: check available space
-		const auto index = m_write.load();
-		const pointer next_index = (index + 1) % N;
-		m_buffer[index] = std::move(value);
-		m_write.store(next_index);
-		m_full = next_index == m_read.load();
+		if (m_full) {
+			return false;
+		}
+
+		const auto write = m_write.load();
+		const auto read = m_read.load();
+		const pointer next_write = (write + 1) % N;
+		m_buffer[write] = std::move(value);
+		m_write.store(next_write);
+		m_full = next_write == read;
+		return true;
 	}
 
-	void pop()
+	bool pop()
 	{
-		// TODO: empty?
-		const auto index = m_read.load();
-		const pointer next_index = (index + 1) % N;
-		m_read.store(next_index);
+		const auto write = m_write.load();
+		const auto read = m_read.load();
+
+		if (!m_full && write == read) {
+			return false;
+		}
+
+		const pointer next_read = (read + 1) % N;
+		m_read.store(next_read);
 		m_full = false;
+		return true;
 	}
 
 	// operator=
-	// constructors
 	// back?
 	// swap?
 	// peek?
@@ -93,8 +118,8 @@ private:
 	std::array<T, N> m_buffer;
 	std::atomic<pointer> m_write{0};
 	std::atomic<pointer> m_read{0};
-	bool m_full = false;
+	std::atomic<bool> m_full{false};
 };
-}  // namespace ecl_common
+}  // namespace ecl::common
 
 #endif
--- common/tests/test_fifo.cpp
+++ common/tests/test_fifo.cpp
@@ -1,10 +1,36 @@
 
+#include <atomic>
 #include <catch2/catch_all.hpp>
 
 #include "catch2/catch_test_macros.hpp"
 #include "containers/fifo.hpp"
 
-using namespace ecl_common;
+using namespace ecl::common;
+
+static_assert(std::is_default_constructible_v<Fifo<uint8_t, 6>>);
+// static_assert(std::is_trivially_constructible_v<Fifo<uint8_t, 6>>);
+static_assert(std::is_nothrow_constructible_v<Fifo<uint8_t, 6>>);
+
+static_assert(std::is_default_constructible_v<Fifo<uint8_t, 6>>);
+// static_assert(std::is_trivially_default_constructible_v<Fifo<uint8_t, 6>>);
+static_assert(std::is_nothrow_default_constructible_v<Fifo<uint8_t, 6>>);
+
+static_assert(std::is_copy_constructible_v<Fifo<uint8_t, 6>> == false);
+static_assert(std::is_trivially_copy_constructible_v<Fifo<uint8_t, 6>> ==
+			  false);
+static_assert(std::is_nothrow_copy_constructible_v<Fifo<uint8_t, 6>> == false);
+
+// static_assert(std::is_move_constructible_v<Fifo<uint8_t, 6>>);
+// static_assert(std::is_trivially_move_constructible_v<Fifo<uint8_t, 6>>);
+// static_assert(std::is_nothrow_move_constructible_v<Fifo<uint8_t, 6>>);
+
+static_assert(std::is_copy_assignable_v<Fifo<uint8_t, 6>> == false);
+static_assert(std::is_trivially_copy_assignable_v<Fifo<uint8_t, 6>> == false);
+static_assert(std::is_nothrow_copy_assignable_v<Fifo<uint8_t, 6>> == false);
+
+// static_assert(std::is_move_assignable_v<Fifo<uint8_t, 6>>);
+// static_assert(std::is_trivially_move_assignable_v<Fifo<uint8_t, 6>>);
+// static_assert(std::is_nothrow_move_assignable_v<Fifo<uint8_t, 6>>);
 
 SCENARIO("FIFO: initialization")
 {
@@ -27,7 +53,7 @@ SCENARIO("FIFO: initialization")
 	}
 }
 
-SCENARIO("FIFO: pushing data")
+SCENARIO("FIFO: adding data")
 {
 	GIVEN("an empty FIFO")
 	{
@@ -35,8 +61,12 @@ SCENARIO("FIFO: pushing data")
 
 		WHEN("pushing a single element")
 		{
-			dut.push(1);
+			REQUIRE(dut.push(1));
 
+			THEN("the element is stored")
+			{
+				REQUIRE(dut.front() == 1);
+			}
 			THEN("size is one")
 			{
 				REQUIRE(dut.size() == 1);
@@ -47,26 +77,109 @@ SCENARIO("FIFO: pushing data")
 			}
 		}
 
-		WHEN("filling the FIFO")
+		WHEN("pushing data untill full")
 		{
-			for (int i = 0; i < 6; ++i) {
-				dut.push(i + 1);
-				REQUIRE(dut.size() == (i + 1));
+			for (int i = 0; i < dut.capacity(); ++i) {
+				REQUIRE(dut.push(i + 1));
+
+				THEN("the size increases")
+				{
+					REQUIRE(dut.size() == (i + 1));
+				}
+				THEN("the front remains intact")
+				{
+					REQUIRE(dut.front() == 1);
+				}
+			}
+
+			THEN("size matches capacity")
+			{
+				REQUIRE(dut.size() == dut.capacity());
+			}
+			THEN("fifo is not empty")
+			{
 				REQUIRE_FALSE(dut.empty());
+			}
+			THEN("pushing a new entry fails")
+			{
+				REQUIRE_FALSE(dut.push(9));
 			}
 		}
 	}
 }
 
-SCENARIO("FIFO: popping data")
+SCENARIO("FIFO: removing data")
 {
 	GIVEN("an empty FIFO")
 	{
 		Fifo<uint8_t, 6> dut{};
+
+		WHEN("popping data")
+		{
+			REQUIRE_FALSE(dut.pop());
+			THEN("fifo remains empty")
+			{
+				REQUIRE(dut.size() == 0);
+				REQUIRE(dut.empty());
+			}
+		}
+	}
+
+	GIVEN("a FIFO contating data")
+	{
+		Fifo<uint8_t, 6> dut{};
+		for (int i = 0; i < dut.capacity(); ++i) {
+			dut.push(i + 1);
+		}
+
+		WHEN("front is called")
+		{
+			THEN("the oldest entry is retruned")
+			{
+				REQUIRE(dut.front() == 1);
+			}
+			THEN("size remains unchanged")
+			{
+				REQUIRE(dut.size() == dut.capacity());
+			}
+		}
+
+		WHEN("pop is called")
+		{
+			REQUIRE(dut.pop());
+
+			THEN("the oldest entry is removed")
+			{
+				REQUIRE(dut.front() == 2);
+			}
+			THEN("size decreases")
+			{
+				REQUIRE(dut.size() < dut.capacity());
+			}
+		}
+
+		WHEN("pop is called until empty")
+		{
+			REQUIRE(dut.size() == 6);
+
+			for (int i = 0; i < dut.capacity(); ++i) {
+				THEN("front retruns the oldest entry")
+				{
+					REQUIRE(dut.front() == (i + 1));
+				}
+
+				REQUIRE(dut.pop());
+
+				THEN("size decreases")
+				{
+					REQUIRE(dut.size() < dut.capacity());
+				}
+			}
+
+			THEN("FIFO is empty")
+			{
+				REQUIRE(dut.empty());
+			}
+		}
 	}
 }
-
-SCENARIO("FIFO: iterating") {}
-
-// back inserter
-// iterator
