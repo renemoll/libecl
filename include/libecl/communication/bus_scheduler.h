dc0287eb7ca26724e97bdaabad10684ce30d63b0
Rene Moll <github@r-moll.nl>
2025-03-03 22:15:31 +0100

    feat: add license

--- include/libecl/communication/bus_scheduler.h
+++ include/libecl/communication/bus_scheduler.h
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-FileCopyrightText: 2025 René Moll
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 #ifndef ECL_COMMUNICATION_BUS_SCHEDULER_H
 #define ECL_COMMUNICATION_BUS_SCHEDULER_H
 
