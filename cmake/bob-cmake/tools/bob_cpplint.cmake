ab40fab3a855a71f84d26a69b25ff6c3389152b1
Rene Moll <github@r-moll.nl>
2025-03-24 21:47:03 +0100

    refactor: bob: fail when a requested tool could not tbe found

--- cmake/bob-cmake/tools/bob_cpplint.cmake
+++ cmake/bob-cmake/tools/bob_cpplint.cmake
@@ -14,27 +14,24 @@
 option(BOB_CPPLINT "Execute `cpplint`" Off)
 
 #
+# bob_configure_cpplint
+#
 # Configure `cpplint` for the given `TARGET`.
 #
-
 function(bob_configure_cpplint TARGET)
 	find_program(CPPLINT_EXE cpplint)
 
-	if (BOB_CPPLINT AND NOT CPPLINT_EXE)
-		bob_error("request for cpplint failed as the executable could not be found")
-	endif()
+	if (BOB_CPPLINT)
+		if (NOT CPPLINT_EXE)
+			bob_error("request for cpplint failed as the executable could not be found")
+		endif()
 
-	if (BOB_CPPLINT AND CPPLINT_EXE)
 		bob_info("enabling cpplint for '${TARGET}'")
 
-		set(CPPLINT_OPTIONS
-		)
-		set_property(
-			TARGET
-				${TARGET}
-			PROPERTY
-				CXX_CPPLINT
-				"${CPPLINT_EXE};${CPPLINT_OPTIONS}"
+		set(CPPLINT_OPTIONS "")
+		set_target_properties(${TARGET}
+			PROPERTIES
+				CXX_CPPLINT "${CPPLINT_EXE};${CPPLINT_OPTIONS}"
 		)
 	endif()
 endfunction()
