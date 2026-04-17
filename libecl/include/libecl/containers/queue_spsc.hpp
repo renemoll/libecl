fdf3749b81e903262737183423822fa52def9edc
Rene Moll <github@r-moll.nl>
2026-04-17 22:22:30 +0200

    feat: QueueSpsc: update based on benchmarks

---
+++ libecl/include/libecl/containers/queue_spsc.hpp
@@ -0,0 +1,229 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-FileCopyrightText: 2025 René Moll
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+#ifndef LIBECL_CONTAINERS_QUEUE_SPSC_H
+#define LIBECL_CONTAINERS_QUEUE_SPSC_H
+
+#include "libecl/containers/aligned_storage.hpp"
+
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <new>
+#include <type_traits>
+
+namespace libecl::containers {
+/*!
+ * \brief    Simple SPSC FIFO queue
+ * \tparam T The type to store.
+ * \tparam N Queue capacity.
+ * \todo     Verify move only types
+ * \todo	 Add front with Maybe type
+ *
+ * Single Producer, Single Consumer queue with fixed capacity.
+ * - Fixed capacity.
+ * - Wait-free & lock-free.
+ * - Does not overwrite existing data when full.
+ * - push and pop will not block, but will return false when the queue is full/empty.
+ */
+template <typename T, std::size_t N>
+class QueueSpsc
+{
+public:
+	using value_type = T;
+	using size_type = std::size_t;
+	using pointer = std::add_pointer_t<T>;
+	using const_pointer = std::add_pointer_t<std::add_const_t<value_type>>;
+	using reference = std::add_lvalue_reference_t<value_type>;
+	using const_reference = std::add_lvalue_reference_t<std::add_const_t<value_type>>;
+	using r_reference = std::add_rvalue_reference_t<value_type>;
+
+	QueueSpsc() noexcept = default;
+	QueueSpsc(QueueSpsc const&) = delete;
+	QueueSpsc& operator=(QueueSpsc const&) = delete;
+	QueueSpsc(QueueSpsc&&) = delete;
+	QueueSpsc& operator=(QueueSpsc&&) = delete;
+
+	~QueueSpsc() noexcept
+	{
+		clear();
+	}
+
+	//! \returns The maximum number of elements the queue can store.
+	[[nodiscard]] constexpr size_type capacity() const noexcept
+	{
+		return N;
+	}
+
+	//! \returns True when the queue is empty, false otherwise.
+	[[nodiscard]] bool empty() const noexcept
+	{
+		return m_write == m_read;
+	}
+
+	[[nodiscard]] bool full() const noexcept
+	{
+		const size_type next_write = next_index(m_write);
+		return next_write == m_read;
+	}
+
+	/*!
+	 * \returns A reference to the first element.
+	 * \pre     Queue must not be empty.
+	 * \todo    Replace with Maybe type to avoid potential UB
+	 */
+	// [[nodiscard]] reference front() noexcept
+	// {
+	// 	assert(!empty());
+	// 	return *m_storage[m_read].data();
+	// }
+
+	/*!
+	 * \returns A constant reference to the first element.
+	 * \pre     Queue must not be empty.
+	 * \todo    Replace with Maybe type to avoid potential UB
+	 */
+	// [[nodiscard]] const_reference front() const noexcept
+	// {
+	// 	assert(!empty());
+	// 	return *m_storage[m_read].data();
+	// }
+
+	/*!
+	 * \brief   Pushes the given \a value into an empty slot within the queue.
+	 * \param   value The value to store.
+	 * \returns True when \a valve is accepted, false if the queue is full.
+	 */
+	template <typename U = T>
+		requires(std::is_nothrow_copy_constructible_v<U>)
+	bool push(const_reference value) noexcept
+	{
+		const size_type write_index = m_write.load(std::memory_order_relaxed);
+		const size_type next_write = next_index(write_index);
+
+		if (next_write == m_read_cache) [[unlikely]] {
+			m_read_cache = m_read.load(std::memory_order_acquire);
+			if (next_write == m_read_cache) [[unlikely]] {
+				return false;
+			}
+		}
+
+		m_storage[write_index].store(value);
+		m_write.store(next_write, std::memory_order_release);
+		return true;
+	}
+
+	/*!
+	 * \brief   Pushes the given \a value into an empty slot within the queue.
+	 * \param   value The value to store.
+	 * \returns True when \a valve is accepted, false if the queue is full.
+	 */
+	template <typename U = T>
+		requires(std::is_nothrow_move_constructible_v<U>)
+	bool push(r_reference value) noexcept
+	{
+		const size_type write_index = m_write.load(std::memory_order_relaxed);
+		const size_type next_write = next_index(write_index);
+
+		if (next_write == m_read_cache) [[unlikely]] {
+			m_read_cache = m_read.load(std::memory_order_acquire);
+			if (next_write == m_read_cache) [[unlikely]] {
+				return false;
+			}
+		}
+
+		m_storage[write_index].store(std::forward<value_type>(value));
+		m_write.store(next_write, std::memory_order_release);
+		return true;
+	}
+
+	/*!
+	 * \brief   Constructs a new object, in-place, in an empty slot within the queue.
+	 * \param   args The arguments to the new object.
+	 * \returns A pointer to the newly created object when space was available, nullptr otherwise.
+	 */
+	template <typename... Args>
+		requires(std::is_nothrow_constructible_v<T, Args...>)
+	pointer emplace(Args&&... args) noexcept
+	{
+		const size_type write_index = m_write.load(std::memory_order_relaxed);
+		const size_type next_write = next_index(write_index);
+
+		if (next_write == m_read_cache) [[unlikely]] {
+			m_read_cache = m_read.load(std::memory_order_acquire);
+			if (next_write == m_read_cache) [[unlikely]] {
+				return nullptr;
+			}
+		}
+
+		m_storage[write_index].emplace(std::forward<Args>(args)...);
+		m_write.store(next_write, std::memory_order_release);
+		return m_storage[write_index].data();
+	}
+
+	//! \returns True when the last element is removed from the queue.
+	template <typename U = T>
+		requires(std::is_nothrow_move_constructible_v<U>)
+	bool pop(value_type& value) noexcept
+	{
+		const size_type read_index = m_read.load(std::memory_order_relaxed);
+		if (read_index == m_write_cache) [[unlikely]] {
+			m_write_cache = m_write.load(std::memory_order_acquire);
+			if (read_index == m_write_cache) [[unlikely]] {
+				return false;  // queue is empty
+			}
+		}
+
+		value = std::move(*m_storage[read_index].data());
+		m_storage[read_index].destroy();
+
+		const size_type next_read = next_index(read_index);
+		m_read.store(next_read, std::memory_order_release);
+		return true;
+	}
+
+	//! \brief Resets the queue, any stored objects are destroyed.
+	void clear()
+	{
+		if (!std::is_trivially_destructible_v<value_type>) {
+			size_type read_index = m_read;
+			size_type write_index = m_write;
+			while (read_index != write_index) {
+				m_storage[read_index].destroy();
+				read_index = next_index(read_index);
+			}
+		}
+
+		m_read = 0;
+		m_write = 0;
+		m_read_cache = 0;
+		m_write_cache = 0;
+	}
+
+private:
+	// Note: m_storage has size `N + 1` to allow differentiation between full and empty.
+	std::array<AlignedStorage<value_type>, N + 1> m_storage = {};
+
+	alignas(std::hardware_destructive_interference_size) std::atomic<size_type> m_write{0};
+	alignas(std::hardware_destructive_interference_size) std::atomic<size_type> m_read{0};
+	alignas(std::hardware_destructive_interference_size) std::atomic<size_type> m_write_cache{0};
+	alignas(std::hardware_destructive_interference_size) std::atomic<size_type> m_read_cache{0};
+
+	[[nodiscard]] std::size_t next_index(std::size_t index) const noexcept
+	{
+		const auto next = index + 1;
+		if (next == m_storage.size()) [[unlikely]] {
+			return 0;
+		}
+		return next;
+	}
+};
+}  // namespace libecl::containers
+
+#endif
